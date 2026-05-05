62cdb0a6a97fc9ffef12923986bcc8c6c9d92a70
NHeadson <nheadson@my.wctc.edu>
2026-05-05 00:41:49 -0500

    further improve template

---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen">
+    <header class="p-4 border-b">App Header</header>
+
+    <main class="p-4">
+      <slot />
+    </main>
+  </div>
+</template>
--- composables/useFetchExample.ts
+++
@@ -1,3 +0,0 @@
-export const useUseFetchExample = () => {
-  return ref()
-}
